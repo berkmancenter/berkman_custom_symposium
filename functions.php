bb43ae2a3ead1a3ad59bec5ec5a13e58e1f9104c
Daniel Collis-Puro <dan@collispuro.net>
2011-04-11 14:17:44 -0400

    Significant updates to style and some very basic javascript.

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php 
+wp_enqueue_script('jquery', 'jquery');
+wp_enqueue_script('hyperpublic.js', get_bloginfo('stylesheet_directory') . '/hyperpublic.js');
+?>
---
+++ hyperpublic.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+	jQuery('#colophon').html(jQuery('.menu').html());
+	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span>');
+	
+});
