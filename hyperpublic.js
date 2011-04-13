a77655bc7d105c1b66194ec99c70c44832bf51b0
Daniel Collis-Puro <dan@collispuro.net>
2011-04-13 15:50:26 -0400

    Fix the custom menu. Make the logo clickable.

--- hyperpublic.js
+++ hyperpublic.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function(){
-	jQuery('#colophon').html(jQuery('.menu').html());
+
+	jQuery('#colophon').html(jQuery('.menu-header').html());
+
 	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span>');
+
+	jQuery('#site-title').bind({
+		click: function(){
+			//TODO: fix this.
+			window.location.href = '/hyperpublic';
+		},
+		mouseover: function(){
+			jQuery(this).css({cursor: 'pointer'});
+		}
+	});
 	
 });
