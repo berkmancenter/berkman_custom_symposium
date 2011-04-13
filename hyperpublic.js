b41feef19dbccb38aecef9edd50e5e988a70b1fd
Daniel Collis-Puro <dan@collispuro.net>
2011-04-13 16:08:47 -0400

    Link to the header.

--- hyperpublic.js
+++ hyperpublic.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function(){
 
 	jQuery('#colophon').html(jQuery('.menu-header').html());
 
-	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span>');
+	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span><br/><span><a href="http://www.harvard.edu/">HARVARD UNIVERSITY</a></span>');
 
 	jQuery('#site-title').bind({
 		click: function(){
-			//TODO: fix this.
+			//TODO: fix this when we start working under the "real" domain.
 			window.location.href = '/hyperpublic';
 		},
 		mouseover: function(){
