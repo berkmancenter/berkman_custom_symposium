2cb5dc0e703cca32937ee437403739889e0cf666
Daniel Collis-Puro <dan@collispuro.net>
2011-04-13 16:38:30 -0400

    Add berkman logo.

--- hyperpublic.js
+++ hyperpublic.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
 
 	jQuery('#colophon').html(jQuery('.menu-header').html());
 
-	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span><br/><span><a href="http://www.harvard.edu/">HARVARD UNIVERSITY</a></span>');
+	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span><br/><span><a href="http://www.harvard.edu/">HARVARD UNIVERSITY</a></span><a href="http://cyber.law.harvard.edu"><img src="/hyperpublic/wp-content/themes/berkman_custom_symposium/images/3d_web_shield.png" alt="Berkman Center for Internet &amp; Society" /></a>');
 
 	jQuery('#site-title').bind({
 		click: function(){
