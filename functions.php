276918b72edf65bc900b05272d237e23a9ebf650
Daniel Collis-Puro <dan@collispuro.net>
2011-05-31 14:16:37 -0400

    Homepage slideshow.

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php 
 wp_enqueue_script('jquery', 'jquery');
+wp_enqueue_script('jquerycycle', get_bloginfo('stylesheet_directory') . '/jquery.cycle.all.min.js');
 wp_enqueue_script('hyperpublic.js', get_bloginfo('stylesheet_directory') . '/hyperpublic.js');
 ?>
--- hyperpublic.js
+++ hyperpublic.js
@@ -4,6 +4,22 @@ jQuery(document).ready(function(){
 
 	jQuery('#site-description').html('<span>A SYMPOSIUM ON DESIGNING PRIVACY AND PUBLIC SPACE</span><br/><span>IN THE CONNECTED WORLD</span><br/><span>JUNE 9–10, 2011</span><br/><span><a href="http://www.harvard.edu/">HARVARD UNIVERSITY</a></span>');
 
+	if(jQuery('body.home').length > 0){
+		// This URL works only on the homepage. Need a better pattern if we're going to use this elsewhere on the site.
+		jQuery.ajax({
+			type: 'GET',
+			cache: false,
+			url: 'speakers/',
+			success: function(html){
+				var interiorHTML = jQuery(html).find('ul.participants-items').html();
+				jQuery('ul.xoxo').prepend('<li class="widget-container"><h3 class="widget-title">Participants</h3><ul class="slideshow">' + interiorHTML + '</ul></li>').find('ul.slideshow').cycle();
+			},
+			complete: function(){
+				jQuery('ul.slideshow a').attr('href','http://www.hyperpublic.org/speakers/');
+			}
+		});
+	}
+
 	jQuery('#site-title').bind({
 		click: function(){
 			//TODO: fix this when we start working under the "real" domain.
