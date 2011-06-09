f3ab6f37c1a0e4137643ec758f5669ca843a9bc0
Daniel Collis-Puro <dan@collispuro.net>
2011-06-09 13:06:19 -0400

    Add twitter widget shortcode to use in sidebar.

--- functions.php
+++ functions.php
@@ -2,4 +2,44 @@
 wp_enqueue_script('jquery', 'jquery');
 wp_enqueue_script('jquerycycle', get_bloginfo('stylesheet_directory') . '/jquery.cycle.all.min.js');
 wp_enqueue_script('hyperpublic.js', get_bloginfo('stylesheet_directory') . '/hyperpublic.js');
+
+function hyperpublic_twitter_shortcode( $atts ) {
+		return "<script src='http://widgets.twimg.com/j/2/widget.js'></script> 
+              <script type='text/javascript' language='javascript'> 
+new TWTR.Widget({
+  version: 2,
+  type: 'search',
+  search: '#hyperpublic',
+  interval: 6000,
+  title: 'Hyperpublic',
+  subject: '#hyperpublic ',
+  width: 188,
+  height: 300,
+  theme: {
+    shell: {
+      background: '#5FA1D0',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#ffffff',
+      color: '#444444',
+      links: '#1985b5'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: true,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: true,
+    behavior: 'default'
+  }
+}).render().start();
+</script>";
+}
+add_shortcode( 'hyperpublic_twitter', 'hyperpublic_twitter_shortcode' );
+add_filter('widget_text', 'do_shortcode');
+
+
 ?>
